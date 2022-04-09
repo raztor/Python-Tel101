1b368800cb40d178bc9c468e8deadb03c9d5e778
benjamin <benjamin@webizy.cl>
2022-04-08 23:13:05 -0400

    codigo no finalizado (C1)

--- proyectos-clase/elecciones.py
+++ proyectos-clase/elecciones.py
@@ -50,3 +50,42 @@ elif Atot==Ctot: #empate A y C
     print('Empate: A y C')
 elif Btot==Ctot: #empate B y C
     print('Empate: B y C')
+
+
+
+
+
+
+
+
+
+
+
+
+
+##codigo por Raztorr
+qtyparticipantes=int(input('Ingrese la cantidad de participantes: '))
+numeroparticipante=0
+totallanza=0
+ganador='N/A'
+distmax=0
+contdist=0
+while qtyparticipantes>numeroparticipante:
+    distancia=0
+    print()
+    print('Participante '+ str(numeroparticipante+1))
+    numeroparticipante=numeroparticipante+1
+    nombre=str(input('Ingrese el nombre del participante: '))
+    promedio=0
+    intento=0
+    while distancia>-1:
+        if intento>=1:
+            promedio = ((float(promedio) + float(distancia)) / 2)
+        distancia=float(input('Ingrese la distancia: '))
+        if intento==0:
+            promedio=distancia
+        intento=intento+1
+
+    print('El promedio de '+nombre+' es '+str(promedio))
+print('Total de lanzamientos vàlidos: ' + str(totallanza))
+print('El participante ganador es '+ganador+ ' con una distancia de '+ str(distmax))
