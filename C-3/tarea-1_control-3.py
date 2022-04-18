405e10ac2e12a9aa66081bc59caf24bc917e533c
benjamin <benjamin@webizy.cl>
2022-04-18 11:09:33 -0400

    código tarea-1_control-3.py finalizado

--- C-3/tarea-1_control-3.py
+++ C-3/tarea-1_control-3.py
@@ -23,4 +23,21 @@ def efectiviad(t1,t2):
 def ataque(fuerza,t1,t2):
     total=efectiviad(t1,t2)*fuerza
     return total
-#def duelo(pkm1, fuerza1, t1, pkm2, fuerza2, t2):
+def duelo(pkm1, fuerza1, t1, pkm2, fuerza2, t2):
+    attack1=fuerza1*efectiviad(t1,t2)
+    attack2=fuerza2*efectiviad(t2,t1)
+    if attack1>attack2:
+        return pkm1
+    elif attack1<attack2:
+        return pkm2
+    elif attack1==attack2:
+        return 'Empate'
+
+pkm1=str(input('pkm1'))
+att1=float(input('att1'))
+t1=str(input('t1'))
+pkm2=str(input('pkm2'))
+att2=float(input('att2'))
+t2=str(input('t2'))
+
+print(duelo(pkm1, att1, t1, pkm2, att2, t2))
