9e72eadab0310be71c74591e087e62441ca53816
Benjamín <benjamin@webizy.cl>
2022-04-23 21:02:59 -0400

    creacion de GYM.py

---
+++ Ayudante/GYM.py
@@ -0,0 +1,55 @@
+import random
+print("Bienvenid@s al GimnasIWI!")
+MC= int(input("Ingrese meta en calorias: "))
+fin=0
+totalcal=0
+while fin==0:
+    if MC<totalcal:
+        print('************\nMeta cumplida! Quemaste', str(totalcal), 'de un total de', str(MC), 'calorias')
+        fin=1
+
+    else:
+        repeticiones=0
+        ejer=0
+        numeroejercicio=int(input('Ingrese ejercicio\n(1) Sentadillas sumo\n(2) Plancha abdominal\n(3) Press frances\n(4) Me canse\n'))
+        if numeroejercicio==1:
+            nombreejercicio='SENTADILLAS SUMO'
+        elif numeroejercicio==2:
+            nombreejercicio='PLANCHA ABDOMINAL'
+        elif numeroejercicio==3:
+            nombreejercicio='PRESS FRANCES'
+        elif numeroejercicio==4:
+            print('************\nA descansar! Quemaste', str(totalcal), 'calorias')
+            ejer=1
+            fin=1
+        else:
+            print('Ingrese una opcion valida')
+            ejer=1
+        while ejer==0:
+            print(nombreejercicio)
+            if numeroejercicio==1:
+                repeticiones=int(input('¿Cuantas repeticiones?: '))
+                cal = random.randint(3, 7)
+                caltot=repeticiones*cal
+                print('Calorias quemadas con sentadillas: ',str(caltot))
+                totalcal+=caltot
+                print('Calorias hasta el momento: ', str(totalcal))
+                ejer = 1
+            elif numeroejercicio==2:
+                sumat=1
+                valsumat=0
+                repeticiones = int(input('¿Cuantos segundos?: '))
+                while sumat<repeticiones:
+                    fact=1
+                    valorfact=1
+                    while fact<=sumat:
+                        valorfact=valorfact*fact #esto da el total del factorial
+                        fact=fact+1
+                    valsumat=valsumat+((4**sumat)/valorfact)
+                    sumat += 1
+                totalcal+=round(valsumat)
+                print('Calorias quemadas con sentadillas: ',str(round(valsumat)))
+                print('Calorias hasta el momento: ', str(totalcal))
+                ejer = 1
+            elif numeroejercicio==3:
+                repeticiones = int(input('¿Cuantas repeticiones?: '))
