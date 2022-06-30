af6a8363bf6aaeaed8a4f2391ee8a51264e0acb4
Benjamin Munoz (Raztorr) <contacto@webizy.cl>
2022-06-30 01:17:18 -0400

    intento de arreglar codigo Ej-4(paises).py

--- Ejercicios/C-5/Guia-12/Ej-4/Ej-4_(Paises).py
+++ Ejercicios/C-5/Guia-12/Ej-4/Ej-4_(Paises).py
@@ -1,51 +1,35 @@
 import matplotlib.pyplot as pp
-import matplotlib.ticker as ticker
 
 def poblacion_paises(nombre_archivo, region):
-    paises_total = []
     total = {}
-    fechas = {}
-    pobla = {}
+    tot_paises = []
     archivo = open(nombre_archivo, 'r', encoding='UTF8')
     for linea in archivo:
-        ano, pais, poblacion, zona = linea.rstrip().split(',')
+        ano, pais, poblacion, zona = linea.strip().split(',')
         poblacion = int(poblacion)
+        ano = int(ano)
         if zona == region:
-            if pais in total:
+            if pais not in total:
+                total[pais] = []
                 total[pais].append((ano, poblacion))
-            else:
-                total[pais] = [(ano, poblacion)]
 
-            if pais not in paises_total:
-                paises_total.append(pais)
     archivo.close()
-    for item in total:
-        total[item].sort()
-    for country in total:
-        for linea in total[country]:
-            date, pop = linea
-            if country in fechas:
-                fechas[country].append(date)
-                pobla[country].append(pop)
-            else:
-                fechas[country] = [date]
-                pobla[country] = [pop]
 
-    fig, ax = pp.subplots(1, 1)
-    for item in reversed(paises_total):
-        print(pobla[item])
-        ax.plot(fechas[item], pobla[item])
+    for pais_for in total:
+        tot_paises.append(pais_for)
+        total[pais_for].sort()
+        x = []
+        y = []
+        for ano_algo, poblacion_total in total[pais_for]:
+            x.append(ano_algo)
+            y.append(poblacion_total)
+        pp.plot(x,y,label=pais_for)
 
-    '''Use esto, ya que se me ploteaban todos los años en el eje y no se leia nada'''
-    ax.xaxis.set_major_locator(ticker.AutoLocator())
-    ax.xaxis.set_minor_locator(ticker.AutoLocator())
-
-    pp.xlabel("Eje x")
-    pp.ylabel("Eje y")
+    pp.legend()
     pp.show()
 
 
-    return paises_total
+    return tot_paises
 
 
 print(poblacion_paises("paises.txt", "Latin America"))
